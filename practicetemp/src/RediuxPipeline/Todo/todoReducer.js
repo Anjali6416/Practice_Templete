9a99ab7fe13f57136cf0d90d535cd3890702742e
Anjali <anjaliprajapati1772001@gmail.com>
2022-12-31 16:38:48 +0530

    added

--- practicetemp/src/RediuxPipeline/Todo/todoReducer.js
+++ practicetemp/src/RediuxPipeline/Todo/todoReducer.js
@@ -1,5 +1,24 @@
+import { ADD, TOGGLE, DELETE } from "./actionType";
 
-const todoReducer= (state,action)=>{
-    
-}
+const initstate = [];
+const todoReducer = (state = initstate, { type, payload }) => {
+  switch (type) {
+    case ADD: {
+      return [...state, payload];
+    }
+    case TOGGLE: {
+      return state.map((e, i) => {
+        if (e.id === payload) {
+          e.status = !e.status;
+        }
+        return e;
+      });
+    }
 
+    case DELETE: {
+      return state.filter((e, i) => {
+        return e.id !== payload;
+      });
+    }
+  }
+};
