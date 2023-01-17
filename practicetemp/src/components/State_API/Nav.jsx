e69db8f153e5d0418d4578dc157c4d43c9e40fe4
Anjali <anjaliprajapati1772001@gmail.com>
2023-01-17 18:52:50 +0530

    api

--- practicetemp/src/components/State_API/Nav.jsx
+++ practicetemp/src/components/State_API/Nav.jsx
@@ -2,6 +2,7 @@ import "./Nav.css"
 function Nav() {
   return (
     <div className="nav">
+        <h1>nav</h1>
       <a href="#">Home</a>
       <a href="#">Profile</a>
       <a href="#">Users</a>
