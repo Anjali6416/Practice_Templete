3bdca00d57a1a5b89fbe1cfb302435c05aa9e1fe
Anjali <anjaliprajapati1772001@gmail.com>
2023-01-17 19:03:52 +0530

    api

--- practicetemp/src/components/State_API/Profile.jsx
+++ practicetemp/src/components/State_API/Profile.jsx
@@ -1,8 +1,36 @@
 
 function Profile(){
     return(
+        <div className="card">
         <div>
-
+         <img src="" alt="" width="100%" height="100%" />
+        </div>
+        <div>
+         <dl>
+            <dt>
+                <b>User Name</b>
+            </dt>
+            <dd></dd>
+            <dt>
+                <b>City</b>
+            </dt>
+            <dd></dd>
+            <dt>
+                <b> Gender</b>
+            </dt>
+            <dd></dd>
+            <dt>
+                <b>Description</b>
+            </dt>
+            <dd>
+            Fugiat dolorum a velit aut? Suscipit, similique soluta. Quas debitis eos aspernatur ex libero. Dolores at, numquam, quas excepturi temporibus veniam, in sunt quia deserunt quam dolorum harum nesciunt porro.
+            Et unde quia ullam cupiditate iusto tempore obcaecati, porro ea ducimus libero, maiores mollitia fuga, blanditiis optio eaque explicabo temporibus molestias repellat assumenda deleniti? Recusandae nam repellat quaerat distinctio quo!
+            Dolorem officia mollitia amet unde eligendi! Odio quisquam harum debitis officia inventore aliquid nisi maxime doloribus nostrum vitae! Quam quibusdam culpa fuga aliquam error, a sed quo iure quis. Omnis.
+            Quibusdam laudantium illum blanditiis quos eum, similique porro iure quasi, tempora ducimus sequi id maiores nesciunt, suscipit ipsam earum consequuntur sed quas enim accusantium beatae. Beatae quia eos corrupti eligendi!
+            Iste necessitatibus repudiandae eius aperiam iure adipisci sit consectetur, natus quam ut voluptate blanditiis corporis voluptatibus! Cumque omnis veritatis nulla facilis, non perferendis dolor voluptas, praesentium, et vel corporis! Ad.
+            molestiae velit? Necessitatibus doloremque odit deserunt aliquam aspernatur? Dolorem, sed dolorum!</dd>
+         </dl>
+        </div>
         </div>
     )
 }
