bc60e0604553c6d70bddfb02882a2881726417b5
Anjali <anjaliprajapati1772001@gmail.com>
2023-01-17 18:44:02 +0530

    api

--- practicetemp/src/components/State_API/Nav.jsx
+++ practicetemp/src/components/State_API/Nav.jsx
@@ -1,10 +1,13 @@
-
-function Nav(){
-    return(
-        <div>
-
-        </div>
-    )
+import "./Nav.css"
+function Nav() {
+  return (
+    <div className="nav">
+      <a href="#">Home</a>
+      <a href="#">Profile</a>
+      <a href="#">Users</a>
+      <a href="#">Home</a>
+    </div>
+  );
 }
 
 export default Nav;
