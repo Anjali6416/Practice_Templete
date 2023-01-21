4631dd21592b738938d7536d08c770d2ac95f7cc
Anjali <anjaliprajapati1772001@gmail.com>
2023-01-21 14:48:59 +0530

    timer

--- practicetemp/src/components/Timer App/Timer.jsx
+++ practicetemp/src/components/Timer App/Timer.jsx
@@ -1,16 +1,22 @@
-import { useState } from "react";
+import { useState,useRef } from "react";
 function Timer(){
+
     const[timer,setTimer]=useState(0)
+  const ref1=useRef();
+   let inter;
     const startTimer=()=>{
-        setInterval(()=>{
-          setTimer(timer+1)
+    ref1.current=setInterval(()=>{
+          setTimer((prev)=>prev+1)
         },1000)
+    }
+    const stopTimer=()=>{
+        clearInterval(ref1.current);
     }
     return(
         <div>
             <h1>Timer is {timer}</h1>
             <button onClick={startTimer}>Start</button>&nbsp;
-            <button>Stop</button>
+            <button onClick={stopTimer}>Stop</button>
         </div>
     )
 }
