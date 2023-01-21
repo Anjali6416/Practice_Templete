b87f8e1f7808e1325306a10d47b6b715cd5a2c24
Anjali <anjaliprajapati1772001@gmail.com>
2023-01-21 13:37:30 +0530

    timer

--- practicetemp/src/components/Timer App/Timer.jsx
+++ practicetemp/src/components/Timer App/Timer.jsx
@@ -1,9 +1,15 @@
-
+import { useState } from "react";
 function Timer(){
+    const[timer,setTimer]=useState(0)
+    const startTimer=()=>{
+        setInterval(()=>{
+          setTimer(timer+1)
+        },1000)
+    }
     return(
         <div>
-            <h1>Timer is 0</h1>
-            <button>Start</button>&nbsp;
+            <h1>Timer is {timer}</h1>
+            <button onClick={startTimer}>Start</button>&nbsp;
             <button>Stop</button>
         </div>
     )
