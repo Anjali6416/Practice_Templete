049b27181852f4f5159af1e6e2d0e6a73a111111
Anjali <anjaliprajapati1772001@gmail.com>
2023-01-21 15:17:23 +0530

    form

--- practicetemp/src/components/Form/Form.jsx
+++ practicetemp/src/components/Form/Form.jsx
@@ -1,25 +1,33 @@
-
+import { useRef } from "react";
 function Form(){
+    const nameref=useRef(null)
+    const passwordref=useRef(null)
+    const emailref=useRef(null);
+    
     var formstyle={
         width:"300px",
         padding:"20px",
         margin:"50px auto",
         boxShadow:"0 0 10px black",
     }
+    const submitData=()=>{
+
+    }
     return(
         <div style={formstyle}>
             <form action="">
+                <h1>Register Form</h1>
                 <div>
-                    <input type="text" placeholder="enter name" />
+                    <input type="text" placeholder="enter name" ref={nameref} />
                 </div><br />
                 <div>
-                    <input type="password" placeholder="enter password" />
+                    <input type="password" placeholder="enter password" ref={passwordref} />
                 </div><br />
                 <div>
-                    <input type="text" placeholder="enter email" />
+                    <input type="text" placeholder="enter email" ref={emailref} />
                 </div><br />
                 <div>
-                    <input type="button" value="Register" />
+                    <input type="button" value="Register" onClick={submitData}/>
                 </div>
             </form>
         </div>
